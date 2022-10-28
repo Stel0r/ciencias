9e9e326e72ccb1216e864e43a2df079b6a3389ae
unknown <seherreraj@udistrital.edu.co>
2022-10-28 07:56:27 -0500

    Eliminación agregado

--- src/arboles/ordenamiento/ArbolOrdenamientoB.java
+++ src/arboles/ordenamiento/ArbolOrdenamientoB.java
@@ -5,36 +5,78 @@ import java.util.ArrayList;
 import arboles.Nodo;
 import arboles.binarios.ArbolBinario;
 
-public class ArbolOrdenamientoB <T extends Comparable> extends ArbolBinario<T> {
-	
-	private ArrayList<Nodo> nodos = new ArrayList<Nodo>();
+public class ArbolOrdenamientoB<T extends Comparable> extends ArbolBinario<T> {
 
-	@Override
-	public void agregarNodo(T dato) {
-		NodoOrdenamientoB<T> nuevo = new NodoOrdenamientoB<>(dato);
-		agregarNodo(nuevo);
-	}
+    /**
+     * @return the nodos
+     */
+    public ArrayList<Nodo> getNodos() {
+        return nodos;
+    }
 
-	@Override
-	public void agregarNodo(Nodo<T> nodo) {
-		if(raiz!=null) {
-			raiz.agregarNodo(nodo);
-		}else {
-			raiz = nodo;
-		}
-		nodos.add(nodo);
-	}
+    /**
+     * @param nodos the nodos to set
+     */
+    public void setNodos(ArrayList<Nodo> nodos) {
+        this.nodos = nodos;
+    }
 
-	@Override
-	public void eliminarNodo(Nodo<T> nodo) {
-		Nodo candidato = nodo.getNodosHijos().get(0);
-		if(candidato == null) {
-			candidato = nodo.getNodosHijos().get(1);
-		}else {
-			while(candidato.getNodosHijos().get(1)!=null) {
-				candidato = (Nodo)candidato.getNodosHijos().get(1);
-			}
-		}
-	}
+    private ArrayList<Nodo> nodos = new ArrayList<Nodo>();
+
+    @Override
+    public void agregarNodo(T dato) {
+        NodoOrdenamientoB<T> nuevo = new NodoOrdenamientoB<>(dato);
+        agregarNodo(nuevo);
+    }
+
+    @Override
+    public void agregarNodo(Nodo<T> nodo) {
+        if (raiz != null) {
+            raiz.agregarNodo(nodo);
+        } else {
+            raiz = nodo;
+        }
+        getNodos().add(nodo);
+    }
+    
+    public Nodo buscar(T dato){
+        for (Nodo nodo : nodos) {
+            if(nodo.getDato().equals(dato)){
+                return nodo;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarNodo(Nodo<T> nodo) {
+        Nodo candidato = nodo.getNodosHijos().get(0);
+        if (candidato == null) {
+            candidato = nodo.getNodosHijos().get(1);
+            candidato.setPadre(nodo.getPadre());
+            if (candidato.getPadre() != null) {
+                candidato.getPadre().getNodosHijos().set(candidato.getPadre().getNodosHijos().indexOf(nodo), candidato);
+            }else{
+                this.setRaiz(candidato);
+            }
+            return;
+        } else {
+            while (candidato.getNodosHijos().get(1) != null) {
+                candidato = (Nodo) candidato.getNodosHijos().get(1);
+            }
+        }
+        if (candidato.getPadre().getNodosHijos().indexOf(candidato) == 0) {
+            candidato.getPadre().getNodosHijos().set(0, candidato.getNodosHijos().get(0));
+        } else {
+            candidato.getPadre().getNodosHijos().set(1, candidato.getNodosHijos().get(0));
+        }
+        candidato.setPadre(nodo.getPadre());
+        candidato.setNodosHijos(nodo.getNodosHijos());
+        if (candidato.getPadre() != null) {
+            candidato.getPadre().getNodosHijos().set(candidato.getPadre().getNodosHijos().indexOf(nodo), candidato);
+        }else{
+            this.setRaiz(candidato);
+        }
+    }
 
 }
--- src/main/Tester.java
+++ src/main/Tester.java
@@ -1,11 +1,13 @@
 package main;
 
+import arboles.Nodo;
+import arboles.ordenamiento.ArbolOrdenamientoB;
 import arboles.ordenamiento.NodoOrdenamientoB;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
 		NodoOrdenamientoB<Edificio> a = new NodoOrdenamientoB<Edificio>(new Edificio(10, 50.5, 1 ));
 		NodoOrdenamientoB<Edificio> b = new NodoOrdenamientoB<Edificio>(new Edificio(15, 50.5, 1 ));
 		NodoOrdenamientoB<Edificio> c = new NodoOrdenamientoB<Edificio>(new Edificio(13, 50.5, 1 ));
@@ -15,8 +17,20 @@ public class Tester {
                 NodoOrdenamientoB<Integer> n1 = new NodoOrdenamientoB<>(80);
                 n1.agregarNodo(new NodoOrdenamientoB<>(20));
                 n1.agregarNodo(new NodoOrdenamientoB<>(150));
-                n1.agregarNodo(new NodoOrdenamientoB<>(120));
-                System.out.println(((NodoOrdenamientoB)n1.getNodosHijos().get(1)).getNodosHijos().get(1));
+                n1.agregarNodo(new NodoOrdenamientoB<>(120));*/
+                ArbolOrdenamientoB<Integer> abi = new ArbolOrdenamientoB<>();
+                abi.agregarNodo(20);
+                abi.agregarNodo(150);
+                abi.agregarNodo(120);
+                abi.agregarNodo(-10);
+                abi.agregarNodo(0);
+                abi.agregarNodo(15);
+                abi.agregarNodo(5);
+                abi.agregarNodo(9);
+                
+                abi.eliminarNodo(abi.buscar(20));
+                abi.recorridoInfijo();
+                System.out.println(abi.getRaiz());
 	}
 
 }
